1200ef05962c33156a31cdb824defc296417a543
TacoEric <ericfleischer4@gmail.com>
2025-03-01 22:39:42 +0100

    Fixed the UI

--- Dont Fluke the Flute/Assets/Scripts/SoundTrackManager.cs
+++ Dont Fluke the Flute/Assets/Scripts/SoundTrackManager.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class SoundTrackManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Nokia controls;
+
+    public void ActivateGameControls() 
     {
+    
         
+
+    }
+    public void StartSoundTrack(int songID) 
+    {
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
